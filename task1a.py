8f7b7d5f914d6162e6d64264515a881be0bb750a
martiabr <martin.abrandt@outlook.com>
2020-03-17 11:17:27 +0100

    Add python file for submission

--- task1a.py
+++ task1a.py
@@ -1,8 +1,12 @@
-from sklearn.linear_model import Ridge
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.linear_model import Ridge, LinearRegression
+from sklearn.model_selection import cross_val_score, cross_validate, train_test_split
+from sklearn.metrics import make_scorer, mean_squared_error
 
-input_file = "Homework 1/train.csv"
+input_file = "train.csv"
+output_file = "submission.txt"
 
 # comma delimited is the default
 df = pd.read_csv(input_file, header = 0)
@@ -13,12 +17,53 @@ df = df._get_numeric_data()
 # put the numeric column names in a python list
 numeric_headers = list(df.columns.values)
 # create a numpy array with the numeric values for input into scikit-learn
-numpy_array = df.as_matrix()
+data = df.values
 
-x = numpy_array[:, 2:]
-y = numpy_array[:,1:]
+x = data[:, 2:]
+y = data[:, 1:2]
 
-print(x)
+n = len(data)
+folds = 10
+lambdas = [0.01, 0.1, 1, 10, 100]
 
-clf = Ridge(alpha=1.0)
-clf.fit(x, y)
+mse = make_scorer(mean_squared_error)
+solution = []
+
+# Test with just splitting data for comparison:
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
+clf = Ridge(1)
+clf.fit(X_train, y_train)
+#print(clf.score(X_test, y_test))
+
+# Try with linear regression as well:
+reg = LinearRegression().fit(X_train, y_train)
+#print(reg.score(X_test, y_test))
+
+# Do 10-fold cross validation and calculate average RMSE for each lambda:
+clf = Ridge()
+for l in lambdas:
+    #print("lambda =", l, ":")
+    clf.set_params(alpha=l)
+    #R2s = cross_val_score(clf, x, y, cv=folds)
+    #MSEs = cross_val_score(clf, x, y, cv=folds, scoring=mse)
+    cv_results = cross_validate(clf, x, y, cv=folds, scoring=mse, return_estimator=True)
+    
+    #print("Coefficients:")
+    #for model in cv_results['estimator']:
+        #print(model.coef_)
+    
+    #print("Test MSEs:")
+    MSEs = cv_results['test_score']
+    
+    RMSEs = np.sqrt(MSEs)
+    #print("MSEs:", MSEs)
+    #print("RMSEs:", RMSEs)
+    
+    #print("Average RMSE:", RMSEs.mean())
+    print(RMSEs.mean())
+    #print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    #print()
+    
+    solution.append(RMSEs.mean())
+
+np.savetxt(output_file, solution, fmt='%.4f')
